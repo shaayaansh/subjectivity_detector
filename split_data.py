2f3c93b8bcfd627e084965b76b715e402dd382cd
Shayan Shokri <shayan@Shayans-Air.lan>
2024-09-24 22:25:06 -0400

    split data

--- split_data.py
+++ split_data.py
@@ -5,13 +5,27 @@ import os
 from sklearn.model_selection import train_test_split
 
 data_path = "Data/MPQA/MPQA.pkl"
-
-print(data_path)
+save_file_path = "Data/MPQA"
 
 with open(data_path, "rb") as f:
     data = pickle.load(f)
 
+train_data, test_data = train_test_split(data, test_size=0.2, shuffle=True, random_state=100)
+train_data, val_data = train_test_split(train_data, test_size=0.1, shuffle=True, random_state=100)
 
-print(len(data))
-print("==============")
-print(data[0])
+train_df = pd.DataFrame(train_data)
+test_df = pd.DataFrame(test_data)
+val_df = pd.DataFrame(val_data)
+
+train_df = train_df[["sentence", "answer"]]
+val_df = val_df[["sentence", "answer"]]
+test_df = test_df[["sentence", "answer"]]
+
+train_df["answer"] = train_df["answer"].apply(lambda x: 1 if x == "yes" else 0)
+val_df["answer"] = val_df['answer'].apply(lambda x: 1 if x == "yes" else 0)
+test_df["answer"] = test_df["answer"].apply(lambda x: 1 if x == "yes" else 0)
+
+train_df.to_csv(os.path.join(save_file_path, "train.csv"))
+val_df.to_csv(os.path.join(save_file_path, "val.csv"))
+test_df.to_csv(os.path.join(save_file_path, "test.csv"))
+
